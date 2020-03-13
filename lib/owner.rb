598242d000bcceca18b139aa30739c64601b32ec
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-13 18:35:18 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -35,11 +35,11 @@ def dogs
 end
 
 def buy_cat(name) 
-  cat = Cat.new(name, self)
+   Cat.new(name, self)
 end
 
 def buy_dog(name)
-  dog = Dog.new(name, self)
+   Dog.new(name, self)
 end
 
 def walk_dogs
